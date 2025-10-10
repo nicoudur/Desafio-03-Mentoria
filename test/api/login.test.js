af6802e7535a5745541cdff12b34c4a12d8f57f2
Matheus Viana <matheusviana762z@gmail.com>
2025-10-09 21:17:53 -0300

    Implementou testes de performance na funcionalidade Login

--- test/api/login.test.js
+++ test/api/login.test.js
@@ -10,9 +10,9 @@ describe('Login', () => {
             const bodyLogin = { ...postLogin }
 
             const resposta = await request(process.env.BASE_URL)
-            .post('/users/login')
-            .set('Content-Type', 'application/json')
-            .send(bodyLogin)
+                .post('/users/login')
+                .set('Content-Type', 'application/json')
+                .send(bodyLogin)
 
             expect(resposta.status).to.equal(200);
             expect(resposta.body.token).to.be.a('string');
@@ -25,13 +25,13 @@ describe('Login', () => {
             bodyLogin.username = 'teste'
 
             const resposta = await request(process.env.BASE_URL)
-            .post('/users/login')
-            .set('Content-Type', 'application/json')
-            .send(bodyLogin)
+                .post('/users/login')
+                .set('Content-Type', 'application/json')
+                .send(bodyLogin)
 
             expect(resposta.status).to.equal(400);
             expect(resposta.body.error).to.equal("Usuário não encontrado");
-            
+
         })
 
         it('Deve retornar 400 com o mensagem de Senha inválida', async () => {
@@ -41,13 +41,13 @@ describe('Login', () => {
             bodyLogin.password = 'teste'
 
             const resposta = await request(process.env.BASE_URL)
-            .post('/users/login')
-            .set('Content-Type', 'application/json')
-            .send(bodyLogin)
+                .post('/users/login')
+                .set('Content-Type', 'application/json')
+                .send(bodyLogin)
 
             expect(resposta.status).to.equal(400);
             expect(resposta.body.error).to.equal("Senha inválida");
-            
+
         })
 
         it('Deve retornar 400 com obrigatoriedade de usuario e mensagem de Usuário e senha obrigatórios', async () => {
@@ -57,29 +57,29 @@ describe('Login', () => {
             bodyLogin.username = ''
 
             const resposta = await request(process.env.BASE_URL)
-            .post('/users/login')
-            .set('Content-Type', 'application/json')
-            .send(bodyLogin)
+                .post('/users/login')
+                .set('Content-Type', 'application/json')
+                .send(bodyLogin)
 
             expect(resposta.status).to.equal(400);
             expect(resposta.body.error).to.equal("Usuário e senha obrigatórios");
-            
+
         })
 
         it('Deve retornar 400 com obrigatoriedade de senha e mensagem de Usuário não encontrado', async () => {
 
             const bodyLogin = { ...postLogin }
 
-             bodyLogin.password = ''
+            bodyLogin.password = ''
 
             const resposta = await request(process.env.BASE_URL)
-            .post('/users/login')
-            .set('Content-Type', 'application/json')
-            .send(bodyLogin)
+                .post('/users/login')
+                .set('Content-Type', 'application/json')
+                .send(bodyLogin)
 
             expect(resposta.status).to.equal(400);
             expect(resposta.body.error).to.equal("Usuário e senha obrigatórios");
-            
+
         })
 
     })
